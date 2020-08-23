dc2dbe3d4a7e7792e9c067d30a1d3440d2e69633
Cleber <juninhosete@yahoo.com.br>
2020-08-23 00:27:29 -0300

    projetojakflix

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Alguma coisa, pra testar a fonte!</p>
+    </div>
+  );
+}
+
+export default App;
